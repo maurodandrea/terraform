db0be232f1bf8572b6dc697686f438a135c1c41a
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2023-09-19 11:26:30 +0200

    terraformversion

--- .infrastructure/main.tf
+++ .infrastructure/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 5.16.2"
     }
   }
 
-  required_version = ">= 1.2.0"
+  required_version = ">= 1.5.7"
 }
 
 provider "aws" {
@@ -17,8 +17,4 @@ resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
 
-  tags = {
-    Name = var.instance_name
-  }
 }
-
