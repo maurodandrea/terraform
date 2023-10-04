af968e2677b5c6f57677ca03d28d64513c64f4f1
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2023-10-04 09:47:15 +0200

    ddd

--- prova/config/server.js
+++ prova/config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS',['XAv1+cYcuz21l5dHA7aDlw==','QqgYEaji081vFU477qwuUA==','DQOMFNmMupXrWZhD5KAQDA==','vvG4guYGdgUoO++jpiUEvg==']),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
