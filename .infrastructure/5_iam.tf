12b8c47ffc5e26262b71ea866f40b67809eaf267
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2023-10-04 14:54:11 +0200

    tt

--- .infrastructure/5_iam.tf
+++ .infrastructure/5_iam.tf
@@ -77,31 +77,8 @@ data "aws_iam_policy_document" "ecs-task-execution" {
         "arn:aws:s3:::media-library-s3-strapi-713024823233"
       ]
     }
-
-resource "aws_s3_bucket_policy" "example_bucket_policy" {
-  bucket = "example-bucket"
-
-  policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Principal": {
-          "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${aws_iam_role.example_role.name}"
-        },
-        "Action": [
-          "s3:GetObject",
-          "s3:ListBucket"
-        ],
-        "Resource": [
-          "arn:aws:s3:::example-bucket",
-          "arn:aws:s3:::example-bucket/*"
-        ]
-      }
-    ]
-  })
 }
-}
+
 
 resource "aws_iam_policy" "ecs-task-execution" {
   name   = "WebsiteTaskExecutionPolicies"
