15e5c4ccff866174769e890df9ccb6630a0e7a0d
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2024-01-11 10:57:18 +0100

    aaa
    a

--- apps/strapi-cms/src/api/pre-header/routes/pre-header.ts
+++
@@ -1,7 +0,0 @@
-/**
- * pre-header router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::pre-header.pre-header');
--- apps/strapi-cms/src/api/pre-header/services/pre-header.ts
+++
@@ -1,7 +0,0 @@
-/**
- * pre-header service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::pre-header.pre-header');
