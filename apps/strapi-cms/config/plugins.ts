c58736c29088b11a544f79f204a26aad9a38865a
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2024-01-25 17:31:23 +0100

    Update plugins.ts

--- apps/strapi-cms/config/plugins.ts
+++ apps/strapi-cms/config/plugins.ts
@@ -34,11 +34,11 @@ export default ({ env }: any) => ({
   'update-static-content': {
     enabled: true,
     config: {
-      githubToken: env('GITHUB_PAT', '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'), // accessing personal github token from env file
-      owner: env('REPO_OWNER', 'maurodandrea'), // owner of the repo
-      repo: env('REPO_NAME', 'terraform'), // name of the repo
-      workflowId: env('WORKFLOW_ID', 'deploy_website.yaml'), // workflowId OR filename
-      branch: env('TARGET_BRANCH', 'main'), // branch name
+      githubToken: env('GITHUB_PAT'), // accessing personal github token from env file
+      owner: env('REPO_OWNER'), // owner of the repo
+      repo: env('REPO_NAME'), // name of the repo
+      workflowId: env('WORKFLOW_ID'), // workflowId OR filename
+      branch: env('TARGET_BRANCH'), // branch name
     },
   },
 });
