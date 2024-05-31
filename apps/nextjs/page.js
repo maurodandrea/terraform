85db55d5ef8c75e498377d2c575c3030c69b9a76
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2024-05-31 10:52:24 +0200

    aaa

---
+++ apps/nextjs/page.js
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+
+      </div>
+
+      <div className="  ">
+      <h2>Manchester United is the best team in the world</h2>
+      <br></br>
+      <h2>Team Winners</h2>
+      <br></br>
+      <h2>This is the project we dockerized</h2>
+      </div>
+
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+
+      </div>
+    </main>
+  )
+}
